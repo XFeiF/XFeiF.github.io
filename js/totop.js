439ec22fb5c6caba03364e1102585843ac94aa3c
XFeiF <eva.aeolus@gmail.com>
2018-09-20 23:39:22 +0800

    Site updated: 2018-09-20 23:39:21

--- js/totop.js
+++ js/totop.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+    $(window).scrollTop() > $(window).height() * 0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
 });
 
 $("#rocket").click(function() {
@@ -22,15 +22,66 @@ $("#homelogo").click(function() {
 var articleW = $('#main>article').css("width");
 
 $("#menu-switch").click(function() {
-    if($("#toc-sidebar").hasClass("toc-hide")){
-        setTimeout(function () { 
+    if ($("#toc-sidebar").hasClass("toc-hide")) {
+        setTimeout(function() {
             $("#toc-sidebar").removeClass("toc-hide");
         }, 200);
         $('#main>article').css("width", articleW);
-    }else{
+    } else {
         $("#toc-sidebar").addClass("toc-hide");
-        setTimeout(function () { 
+        setTimeout(function() {
             $('#main>article').css("width", "100%");
         }, 200);
     }
 });
+
+// Directory  
+var $itemHasChild = $("#toc .nav-item:has(> .nav-child)");
+var $titleHasChild = $itemHasChild.children(".nav-link");
+$itemHasChild.prepend("<i class='fa fa-caret-down'></i><i class='fa fa-caret-right'></i>");
+
+var clickIcon = function() {
+    $("#toc .nav-item > i").click(function() {
+        $(this).siblings(".nav-child").slideToggle(100);
+        $(this).toggleClass("hide");
+        $(this).siblings("i").toggleClass("hide");
+    })
+}()
+
+var clickTitle = function() {
+    $titleHasChild.dblclick(function() {
+            $(this).siblings(".nav-child").hide(100);
+            $(this).siblings("i").toggleClass("hide");
+        })
+        // After dblclick enent
+    $titleHasChild.click(function() {
+        var $curentTocChild = $(this).siblings(".nav-child");
+        if ($curentTocChild.is(":hidden")) {
+            $curentTocChild.show(100);
+            $(this).siblings("i").toggleClass("hide");
+        }
+    })
+}()
+
+var clickTocTitle = function() {
+    var $iconToExpand = $(".nav-item > .fa-caret-right");
+    var $iconToFold = $(".nav-item > .fa-caret-down");
+    var $subToc = $titleHasChild.next(".nav-child");
+    $iconToExpand.addClass("hide");
+
+    var $tocTitle = $("#toc .toc-title");
+    if ($titleHasChild.length) {
+        $tocTitle.addClass("clickable");
+        $tocTitle.click(function() {
+            if ($subToc.is(":hidden")) {
+                $subToc.show(150);
+                $iconToExpand.removeClass("hide");
+                $iconToFold.addClass("hide");
+            } else {
+                $subToc.hide(100);
+                $iconToExpand.addClass("hide");
+                $iconToFold.removeClass("hide");
+            }
+        })
+    }
+}()
